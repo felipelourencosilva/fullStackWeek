4be9178effd84ccb075d130884920990ab39d100
Felipe Lourenço <lipelourencosilva@gmail.com>
2023-07-04 20:53:09 -0300

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Trips from "./components/Trips";
+
+export const metadata = {
+  title: "About",
+};
+
+const Page = () => {
+  return (
+    <div>
+      <h1>teste</h1>
+    </div>
+  );
+};
+
+export default Page;
