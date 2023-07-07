5cf272b242b173ef7e0f9be3f6519863399ec731
Felipe Lourenço <lipelourencosilva@gmail.com>
2023-07-07 17:10:43 -0300

    feat: add trip component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
         <Image height={32} width={32} src={data.user.image!} alt={data.user.name! }  className='rounded-full shadow-md'/>
 
         {menuIsOpen && (
-          <div className='absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center'>
+          <div className='z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center'>
             <button className='text-primary text-sm font-semibold' onClick={handleLogoutClick}>Logout</button>
           </div>
         )}
