33512b5720bc196385b6d713e1361e096cd030a5
Felipe Lourenço <lipelourencosilva@gmail.com>
2023-07-07 14:04:44 -0300

    feat: add initial trip details page and trip header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-'use client'
-
 import { signIn, signOut, useSession } from 'Next-auth/react'
 import TripSearch from './components/TripSearch'
 import QuickSearch from './components/QuickSearch'
 import RecommendedTrips from './components/RecommendedTrips'
 
 export default function Home() {
-  const { data } = useSession()
-
   return (
     <div>
       <TripSearch />
