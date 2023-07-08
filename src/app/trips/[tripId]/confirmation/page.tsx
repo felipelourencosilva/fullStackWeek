e935d73fcf30932ceaf6ca0a651023f8d3295339
Felipe Lourenço <lipelourencosilva@gmail.com>
2023-07-08 19:59:09 -0300

    feat: add trip buy

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image'
 import { useSearchParams } from 'next/navigation'
 import ReactCountryFlag from 'react-country-flag'
 import ptBR from 'date-fns/locale/pt-BR'
+import { toast } from 'react-toastify'
 
 import Button from '@/components/Button'
 
@@ -17,7 +18,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const [trip, setTrip] = useState<Trip | null>()
   const [totalPrice, setTotalPrice] = useState<number>(0)
 
-  const { status } = useSession()
+  const { status, data } = useSession()
 
   const router = useRouter()
 
@@ -30,22 +31,21 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
         body: JSON.stringify({
           tripId: params.tripId,
           startDate: searchParams.get('startDate'),
-          endDate: searchParams.get('endDate')
+          endDate: searchParams.get('endDate'),    
         })
       })
 
       const res = await response.json()
 
-      if(res?.error){
+      if (res?.error) {
         return router.push('/')
       }
-
 
       setTrip(res.trip)
       setTotalPrice(res.totalPrice)
     }
 
-    if(status === 'unauthenticated'){
+    if (status === 'unauthenticated') {
       router.push('/')
     }
 
@@ -53,50 +53,90 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   }, [status, searchParams, params.tripId, router])
 
   if (!trip) return null
-  
-  const startDate = new Date(searchParams.get("startDate") as string)
-  const endDate = new Date(searchParams.get("endDate") as string)
-  const guests = searchParams.get("guests")
+
+  const handleBuyClick = async () => {
+    const res = await fetch('http://localhost:3000/api/trips/reservation', {
+      method: 'POST',
+      body: Buffer.from(
+        JSON.stringify({
+          tripId: params.tripId,
+          startDate: searchParams.get('startDate'),
+          endDate: searchParams.get('endDate'),
+          guests: Number(searchParams.get('guests')),
+          userId: (data?.user as any)?.id!,
+          totalPaid: totalPrice,
+        })
+      )
+    })
+
+    if(!res.ok) {
+      return toast.error("Ocorreu um erro ao realizar a reserva!")
+    }
+
+    router.push("/")
+    
+    toast.success('Reserva realizada com sucesso!', {
+      position: 'bottom-center',
+    })
+  }
+
+  const startDate = new Date(searchParams.get('startDate') as string)
+  const endDate = new Date(searchParams.get('endDate') as string)
+  const guests = searchParams.get('guests')
 
   return (
     <div className="container mx-auto p-5">
       <h1 className="font-semibold text-xl text-primaryDarker">Sua viagem</h1>
 
-      <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">  
+      <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
         <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
           <div className="relative h-[106px] w-[124px]">
-            <Image src={trip.coverImage} fill style={{ objectFit: "cover" }} className="rounded-lg" alt={trip.name} />
+            <Image
+              src={trip.coverImage}
+              fill
+              style={{ objectFit: 'cover' }}
+              className="rounded-lg"
+              alt={trip.name}
+            />
           </div>
 
           <div className="flex flex-col">
-            <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
+            <h2 className="text-xl text-primaryDarker font-semibold">
+              {trip.name}
+            </h2>
             <div className="flex items-center gap-1">
               <ReactCountryFlag countryCode={trip.countryCode} svg />
-              <p className="text-xs text-grayPrimary underline">{trip.location}</p>
+              <p className="text-xs text-grayPrimary underline">
+                {trip.location}
+              </p>
             </div>
           </div>
         </div>
-        
-        <h3 className='font-semibold text-lg text-primaryDarker mt-3'>Informações sobre o preço</h3>
+
+        <h3 className="font-semibold text-lg text-primaryDarker mt-3">
+          Informações sobre o preço
+        </h3>
 
         <div className="flex justify-between mt-2">
-          <p className='text-primaryDarker'>Total</p>
-          <p className='font-medium'>R${totalPrice}</p>
+          <p className="text-primaryDarker">Total</p>
+          <p className="font-medium">R${totalPrice}</p>
         </div>
       </div>
 
       <div className="flex flex-col mt-5 text-primaryDarker">
-        <h3 className='font-semibold'>Data</h3>
+        <h3 className="font-semibold">Data</h3>
         <div className="flex items-center gap-1 mt-1">
-          <p>{format(startDate, "dd 'de' MMMM", { locale: ptBR})}</p>
+          <p>{format(startDate, "dd 'de' MMMM", { locale: ptBR })}</p>
           {'-'}
-          <p>{format(endDate, "dd 'de' MMMM", { locale: ptBR})}</p>
+          <p>{format(endDate, "dd 'de' MMMM", { locale: ptBR })}</p>
         </div>
 
-        <h3 className='font-semibold mt-5'>Hóspedes</h3>
+        <h3 className="font-semibold mt-5">Hóspedes</h3>
         <p>{guests} Hóspedes</p>
 
-        <Button className='mt-5'>Finalizar compra</Button>
+        <Button onClick={handleBuyClick} className="mt-5">
+          Finalizar compra
+        </Button>
       </div>
     </div>
   )
